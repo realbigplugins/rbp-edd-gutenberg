fbbc1057897c09232b4a1ea7d8cd0b5766fa52ad
Joel Yoder <joelyoder@realbigmarketing.com>
2021-09-01 14:34:34 -0400

    Initial commit

---
+++ rpb-downloads-fields.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: RBP EDD Gutenberg
+ * Description: Enables the block editor on EDD's "Downloads" post type
+ * Version: 1.0.0
+ * Text Domain: rbp-edd-gutenberg
+ * Author: Real Big Marketing
+ * Author URI: http://realbigmarketing.com
+ * Contributors: joelyoder
+ * GitHub Plugin URI: realbigplugins/rbp-edd-gutenberg
+ * GitHub Branch: master
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    die;
+}
+
